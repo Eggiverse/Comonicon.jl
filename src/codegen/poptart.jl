4e60fb7816a3e892b32dfadfae6356ac88f1fb8d
St Z <abczst@outlook.com>
2020-08-27 15:41:27 +0800

    first working poptart

--- src/codegen/poptart.jl
+++ src/codegen/poptart.jl
@@ -4,13 +4,15 @@
 
 Poptart code generation context.
 """
-mutable struct PoptartCtx <: AbstractCtx
-    ptr::Int
+struct PoptartCtx <: AbstractCtx
+    arg_inputs::Dict{Arg, Symbol}
+    option_inputs::Dict{Option, Symbol}
+    flag_inputs::Dict{Flag, Symbol}
     help::Symbol
     version::Symbol
 end
 
-PoptartCtx() = PoptartCtx(1, gensym(:help), gensym(:version))
+PoptartCtx() = PoptartCtx(Dict{Arg, Symbol}(), Dict{Option, Symbol}(), Dict{Flag, Symbol}(), gensym(:help), gensym(:version))
 
 """
     codegen(cmd)
@@ -25,15 +27,11 @@ function codegen(cmd::AbstractCommand)
 
     ctx = PoptartCtx()
     defs[:body] = quote
-        # let Julia throw InterruptException on SIGINT
-        # ccall(:jl_exit_on_sigint, Cvoid, (Cint,), 0)
-        # $(codegen_scan_glob(ctx, cmd))
         $(codegen(ctx, cmd))
     end
     return quote
-        import Poptart
         using Poptart.Desktop
-        window = Window()
+        window = Window(title=$(cmd.root.name))
         app = Application(windows = [window])
         $(combinedef(defs))
         command_main()
@@ -51,142 +49,114 @@ function codegen(ctx::PoptartCtx, cmd::EntryCommand)
 end
 
 function codegen_body(ctx::PoptartCtx, cmd::LeafCommand)
-    parameters = gensym(:parameters)
-    n_args = gensym(:n_args)
-    nrequires = nrequired_args(cmd.args)
     ret = Expr(:block)
-    validate_ex = Expr(:block)
+    
+    for args in (cmd.args, cmd.options, cmd.flags)
+        expr = codegen_inputs(ctx, args)
+        push!(ret.args, expr)
+    end
 
-    pushmaybe!(ret, codegen_params(ctx, parameters, cmd))
+    button_expr = quote
+        button_run = Button(title = "run")
+        button_cancel = Button(title = "cancel")
+        push!(window.items, button_run, button_cancel)
 
-    # if nrequires > 0
-    #     err = xerror(
-    #         :("command $($(cmd.name)) expect at least $($nrequires) arguments, got $($n_args)"),
-    #     )
-    #     push!(validate_ex.args, quote
-    #         if $n_args < $nrequires
-    #             $err
-    #         end
-    #     end)
-    # end
+        didClick(button_run) do event
+            $(codegen_call(ctx, cmd))
+        end
+    end
+    push!(ret.args, button_expr)
 
-    # # Error: too much arguments
-    # if isempty(cmd.args) || !last(cmd.args).vararg
-    #     nmost = length(cmd.args)
-    #     err = xerror(:("command $($(cmd.name)) expect at most $($nmost) arguments, got $($n_args)"))
-    #     push!(validate_ex.args, quote
-    #         if $n_args > $nmost
-    #             $err
-    #         end
-    #     end)
-    # end
-
-    # push!(ret.args, :($n_args = length(ARGS) - $(ctx.ptr - 1)))
-    # push!(ret.args, validate_ex)
-    push!(ret.args, codegen_call(ctx, parameters, n_args, cmd))
     push!(ret.args, :(return 0))
     return ret
 end
 
-function codegen_params(ctx::PoptartCtx, params::Symbol, cmd::LeafCommand)
-    hasparameters(cmd) || return
-
-    regexes, actions = [], []
-    controls = []
-    arg = gensym(:arg)
-    it = gensym(:index)
-
-    for opt in cmd.options
-        push!(regexes, regex_flag(opt))
-        push!(regexes, regex_option(opt))
-
-        push!(actions, read_forward(params, it, opt))
-        push!(actions, read_match(params, it, opt))
-
-        push!(controls, InputText(label=opt.name, buf=""))
-
-        if opt.short
-            push!(regexes, regex_short_flag(opt))
-            push!(regexes, regex_short_option(opt))
-
-            push!(actions, read_forward(params, it, opt))
-            push!(actions, read_match(params, it, opt))
-        end
-    end
-
-    for flag in cmd.flags
-        push!(regexes, regex_flag(flag))
-        push!(actions, read_flag(params, it, flag))
-
-        push!(controls, InputText(label=flag.name, buf=""))
-
-        if flag.short
-            push!(regexes, regex_short_flag(flag))
-            push!(actions, read_flag(params, it, flag))
-        end
-    end
-
-    return quote
-        append!(window.items, $controls)
-
-        # $params = []
-        # $it = $(ctx.ptr)
-        # while !isempty(ARGS) && $(ctx.ptr) <= $it <= length(ARGS)
-        #     $arg = ARGS[$it]
-        #     if startswith($arg, "-") # is a flag/option
-        #         $(xmatch(regexes, actions, arg))
-        #     else
-        #         $it += 1
-        #     end
-        # end
-     
-    end
-end
-
-function codegen_call(ctx::PoptartCtx, params::Symbol, n_args::Symbol, cmd::LeafCommand)
+function codegen_call(ctx::PoptartCtx, cmd::LeafCommand)
     ex_call = Expr(:call, cmd.entry)
-    if hasparameters(cmd)
-        push!(ex_call.args, Expr(:parameters, :($params...)))
-    end
-
-    # since we will check the position of arguments
-    # the optional and variational arguments are always in the end
-    for (i, arg) in enumerate(cmd.args)
-        if arg.require
-            push!(ex_call.args, xparse_args(arg, ctx.ptr + i - 1))
-        else
-            break
-        end
-    end
-
-    all_required(cmd) && return code_gen_button(ex_call)
-
-    # handle optional arguments
-    ex = Expr(:block)
-    if cmd.nrequire >= 0
-        push!(ex.args, Expr(:if, :($n_args == $(cmd.nrequire)), ex_call))
-    end
-
-    expanded_call_ex = copy(ex_call)
-    for i in cmd.nrequire+1:length(cmd.args)
-        expanded_call_ex = copy(expanded_call_ex)
-        push!(expanded_call_ex.args, xparse_args(cmd.args[i], ctx.ptr + i - 1))
-        push!(ex.args, Expr(:if, :($n_args == $i), expanded_call_ex))
-    end
-
-    @info "generate button_run!"
-    return code_gen_button(ex)
-end
-
-function code_gen_button(ex)
-    quote
-        button_run = Button(title = "run")
-        push!(window.items, button_run)
     
-        didClick(button_run) do event
-            $ex
-        end
+    for (arg, input) in ctx.arg_inputs
+        value = inputvalue(ctx, arg, input)
+        push!(ex_call.args, value)
     end
+
+    for (opt, input) in ctx.option_inputs
+        value = inputvalue(ctx, opt.arg, input)
+        push!(ex_call.args, Expr(:kw, Symbol(opt.name), value))
+    end
+
+    for (flag, input) in ctx.flag_inputs
+        value = :(parse(Bool, $input.buf))
+        push!(ex_call.args, Expr(:kw, Symbol(flag.name), value))
+    end
+    
+    return ex_call
 end
 
-parse_bool(s::String) = (lowercase(s) in ["yes", "true"]) ? true : false
+function inputvalue(::PoptartCtx, arg::Arg, input::Symbol)
+    if arg.type == Any
+        value = :($input.buf)
+    else
+        value = :(parse($(arg.type), $input.buf))
+    end
+    value
+end
+
+"""
+Generate inputs for `args`
+"""
+function codegen_inputs(ctx::PoptartCtx, args)
+    genexpr = Expr(:block)
+    for arg in args
+        expr, input = codegen_input(ctx, arg)
+        push!(genexpr.args, expr, :(push!(window.items, $input)))
+    end
+    return genexpr
+end
+
+function codegen_input(ctx::PoptartCtx, arg)
+    input_symbol = gensym(:input)
+    expr = codegen_input(ctx, input_symbol, arg)
+    push!(ctx, arg=>input_symbol)
+    return expr, input_symbol
+end
+
+function Base.push!(ctx::PoptartCtx, arg_input::Pair{Arg, Symbol})
+    push!(ctx.arg_inputs, arg_input)
+end
+
+function Base.push!(ctx::PoptartCtx, arg_input::Pair{Option, Symbol})
+    push!(ctx.option_inputs, arg_input)
+end
+
+function Base.push!(ctx::PoptartCtx, arg_input::Pair{Flag, Symbol})
+    push!(ctx.flag_inputs, arg_input)
+end
+
+function codegen_input(::PoptartCtx, input::Symbol, arg::Arg)
+    label = arg.name
+    if arg.type != Any
+        label *= "::" * string(arg.type)
+    end
+    if arg.require
+        label *= " *"
+    end
+    :($input = InputText(label=$label, buf=""))
+end
+
+function codegen_input(::PoptartCtx, input::Symbol, opt::Option)
+    label = opt.name 
+    # Probabaly a bad idea to cat strings
+    if opt.arg.type != Any
+        label *= "::" * string(opt.arg.type)
+    end
+    if opt.arg.require
+        label *= " *"
+    end
+    :($input = InputText(label=$label, buf=""))
+end
+
+function codegen_input(::PoptartCtx, input::Symbol, flag::Flag)
+    label = flag.name * "::Bool"
+
+    :($input = InputText(label=$label, buf="false"))
+end
